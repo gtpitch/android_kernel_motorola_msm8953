64fc50cae4a978ba28c87e9fdf8c3a9464c62a4b
Zac Crosby <zac@motorola.com>
2015-10-30 10:46:10 -0500

    msm: mdss: Add method to force cmd TX mode post display on
    The Sheridan display driver IC has a design flaw where it will ignore
    the first LP transmission following an HS transmission.  There are
    multiple work-arounds for the issue, but the best is to send all commands
    (except for display on & off sequences) in HS mode.
    
    To accomplish this, add method to allow panel device tree to specify the
    default TX mode to use for commands.  This mode will override any
    hard-coded TX modes for commands.
    
    Change-Id: I3621888ea51afc09acc8f9c8988bb993b3e30d7d
    Signed-off-by: Zac Crosby <zac@motorola.com>
    Reviewed-on: http://gerrit.mot.com/796096
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_panel.h
+++ drivers/video/msm/mdss/mdss_panel.h
@@ -739,6 +739,8 @@ struct mdss_panel_info {
 
 	u32 disp_on_check_val;
 	bool no_panel_read_support;
+	u32 forced_tx_mode_ftr_enabled;
+	u32 forced_tx_mode_state;
 
 	/* HDR properties of display panel*/
 	struct mdss_panel_hdr_properties hdr_properties;
